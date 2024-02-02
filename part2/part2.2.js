87b9d283918bf9d0b060a5be8c5e56430e9fad2e
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-02 20:02:48 +0000

    feat: adding curves to a path

--- part2/part2.2.js
+++ part2/part2.2.js
@@ -33,20 +33,41 @@ context.fillText("(250, 170)", 255, 175);
 
 /* SECTION 2 */
 
-context.beginPath();
-context.moveTo(50, 250);
-context.lineTo(50, 350);
-context.lineTo(150, 350);
-context.closePath();
-context.moveTo(230, 360);
-context.lineTo(270, 360);
-context.lineTo(270, 310);
-context.closePath();
-context.moveTo(250, 50);
-context.lineTo(370, 50);
-context.lineTo(370, 100);
-context.closePath();
-context.strokeStyle = "#FFFF00";
-context.fillStyle = "#000000";
-context.fill();
-context.stroke();
+// context.beginPath();
+// context.moveTo(50, 250);
+// context.lineTo(50, 350);
+// context.lineTo(150, 350);
+// context.closePath();
+// context.moveTo(230, 360);
+// context.lineTo(270, 360);
+// context.lineTo(270, 310);
+// context.closePath();
+// context.moveTo(250, 50);
+// context.lineTo(370, 50);
+// context.lineTo(370, 100);
+// context.closePath();
+// context.strokeStyle = "#FFFF00";
+// context.fillStyle = "#000000";
+// context.fill();
+// context.stroke();
+
+// Adding Curves to a path
+/* context.quadraticCurveTo */
+
+// context.beginPath();
+// context.moveTo(50, 250);
+// context.quadraticCurveTo(25, 300, 50, 350);
+// context.quadraticCurveTo(100, 375, 150, 350);
+// context.closePath();
+// context.moveTo(230, 360);
+// context.quadraticCurveTo(255, 340, 270, 360);
+// context.quadraticCurveTo(255, 340, 270, 310);
+// context.closePath();
+// context.moveTo(250, 50);
+// context.quadraticCurveTo(310, 60, 370, 50);
+// context.quadraticCurveTo(400, 75, 370, 100);
+// context.closePath();
+// context.strokeStyle = "#FFFF00";
+// context.fillStyle = "#000000";
+// context.fill();
+// context.stroke();
