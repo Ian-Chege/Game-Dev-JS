60be0d897118a49ed7c1b5935349c59d935b95a9
Ian Chege <en20101832020@student.mut.ac.ke>
2024-03-01 17:13:24 +0300

    feat: thruster control

---
+++ part8(controlling-the-ship)/part8.1.js
@@ -0,0 +1,61 @@
+let context = document.getElementById("asteroids").getContext("2d");
+
+let asteroid = new Asteroid(
+  10000,
+  Math.random() * context.canvas.width,
+  Math.random() * context.canvas.height
+);
+
+asteroid.push(Math.random() * 2 * Math.PI, 1000, 60);
+asteroid.twist(Math.random() * 100, 60);
+
+let ship = new Ship(
+  10,
+  15,
+  context.canvas.width / 2,
+  context.canvas.height / 2,
+  1000
+);
+
+function draw() {
+  draw_grid(context);
+  ship.draw(context, true);
+  asteroid.draw(context, true);
+}
+
+function update(elapsed) {
+  asteroid.update(elapsed, context);
+  ship.update(elapsed, context);
+}
+
+// implement the key_handler function here
+
+context.canvas.addEventListener(
+  "keydown",
+  function (e) {
+    key_handler(e, true);
+  },
+  true
+);
+
+context.canvas.addEventListener(
+  "keyup",
+  function (e) {
+    key_handler(e, false);
+  },
+  true
+);
+
+context.canvas.focus();
+
+var previous;
+function frame(timestamp) {
+  if (!previous) previous = timestamp;
+  var elapsed = timestamp - previous;
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  update(elapsed / 1000);
+  draw(context);
+  previous = timestamp;
+  window.requestAnimationFrame(frame);
+}
+window.requestAnimationFrame(frame);
